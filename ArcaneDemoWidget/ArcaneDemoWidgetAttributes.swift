3ec7df014ff4056ae8010502c0c1aa59ba8ab247
Kyle Mendell <ksm@ofkm.us>
2026-05-11 11:22:57 -0500

    refactor: remove live activity

--- ArcaneDemoWidget/ArcaneDemoWidgetAttributes.swift
+++
@@ -1,9 +0,0 @@
-import ActivityKit
-import Foundation
-
-struct ArcaneDemoWidgetAttributes: ActivityAttributes, Sendable {
-    public struct ContentState: Codable, Hashable, Sendable {
-        var startedAt: Date
-        var endsAt: Date
-    }
-}
--- ArcaneDemoWidget/ArcaneDemoWidgetBundle.swift
+++
@@ -1,9 +0,0 @@
-import WidgetKit
-import SwiftUI
-
-@main
-struct ArcaneDemoWidgetBundle: WidgetBundle {
-    var body: some Widget {
-        ArcaneDemoWidgetLiveActivity()
-    }
-}
